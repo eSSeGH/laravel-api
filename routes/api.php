bfee972c2751da670249d81b3bcb1c0b26d070e4
eSSeGH <ste.virgadaula@gmail.com>
2023-05-02 16:13:07 +0200

    MILESTONE 1 api completed

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/projects', [ProjectController::class, 'index']);
+
+
+Route::get('/test', function () {
+    return response()->json([
+        'name' => 'Stefano',
+        'role' => 'admin'
+    ]);
 });
